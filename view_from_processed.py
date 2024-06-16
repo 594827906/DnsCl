389641b6bec068d9f031b3b1247073df3e34ad1e
594827906 <594827906@qq.com>
2024-06-16 23:57:39 +0800

    fix logic of plot tic from csv

--- view_from_processed.py
+++ view_from_processed.py
@@ -9,20 +9,26 @@ def tic_from_csv(file, label, mode):  # 从处理后的csv中绘制TIC图
     df = df.sort_values(['scan', 'mz'])  # 按指定列排序
     max_scan = df.iloc[-1]['scan']  # 获取最大scan数
     min_scan = df.iloc[0]['scan']  # 获取最小scan
-    min_time = df.iloc[0]['RT']  # 获取最小time
-    scan_freq = min_time / min_scan  # 对扫描频率的估算，非常不准
+    max_time = df.iloc[-1]['RT']  # 获取最小time
+    scan_freq = max_time / min_scan  # 对扫描频率的估算，非常不准
     max_scan = max_scan.astype(int)  # 转换为整型
+    time.append(0)
+    tic.append(0)
     for scan in range(1, max_scan):
         if (df['scan'] == scan).any():
             tot_intensity = df.loc[df['scan'] == scan, 'intensity'].sum()  # get total ion of scan
             tic.append(tot_intensity)
             t = df.loc[df['scan'] == scan, 'RT'].values[0]  # get scan time
             time.append(t)
-        # else:
-        #     tic.append(0)
-        #     time.append(scan * scan_freq)
-        # if progress_callback is not None and not i % 10:  # 备用：进度条
-        #     progress_callback.emit(int(i * 100 / max_scan))
+        else:
+            if (df['scan'] == scan+1).any():
+                t = df.loc[df['scan'] == scan + 1, 'RT'].values[0]
+                time.append(t)
+                tic.append(0)
+            if (df['scan'] == scan-1).any():
+                t = df.loc[df['scan'] == scan - 1, 'RT'].values[0]
+                time.append(t)
+                tic.append(0)
 
     del df
     gc.collect()  # 回收内存
